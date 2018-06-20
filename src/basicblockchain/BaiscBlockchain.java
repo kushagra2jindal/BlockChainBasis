6b1267e24a52ecd256b8c5ea1a45461bee6a0e11
kushagra2jindal <kushagra2jindal@gmail.com>
2018-06-20 13:28:57 -0700

    mining the blocks from the main class

--- src/basicblockchain/BaiscBlockchain.java
+++ src/basicblockchain/BaiscBlockchain.java
@@ -2,24 +2,40 @@ package basicblockchain;
 
 import java.util.ArrayList;
 
+/**
+ *
+ * @author kushagrajindal
+ */
 
 public class BaiscBlockchain {
     
+    /**
+     * @param args the command line arguments
+     */
     
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
-    
+    public static int difficulty = 4;  //level of difficulty define how many initial zero are in the hash
     public static void main(String[] args) {
         
         
         // TODO code application logic here
-        blockchain.add(new Block("0", "this is the 1st block"));
+        blockchain.add(new Block("Hi im the first block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
         
-        blockchain.add(new Block(blockchain.get(blockchain.size()-1).hash, "this is the 2nd block"));
+        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
         
-        blockchain.add(new Block(blockchain.get(blockchain.size()-1).hash, "this is the 3rd block"));
+        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+        
+        System.out.println("\nBlockchain is Valid: " + doValidate());
         
         
-        System.out.println (blockchain.get(1).hash);
+        
+        
     }
     
     public static boolean doValidate(){
@@ -36,6 +52,8 @@ public class BaiscBlockchain {
                 if(!prevblock.hash.equals(currentblock.prevHash))
                     return false;
                 
+                if(!currentblock.hash.substring( 0, difficulty).equals(hashTarget))
+                    return false;
                 
             }
             else{
@@ -44,8 +62,11 @@ public class BaiscBlockchain {
                 if(!currentblock.hash.equals(currentblock.createHash()) )
                     return false;
                 
+                if(!currentblock.hash.substring( 0, difficulty).equals(hashTarget))
+                    return false;
             }
         }
         return true;
     }
+    
 }
